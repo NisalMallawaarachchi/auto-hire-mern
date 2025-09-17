a1a852af64165aec86644ad628bb302b74d094af
NisalMallawaarachchi <nisalmallawarachchi@gmail.com>
2025-09-17 20:23:04 +0530

    feat: enhance App component to conditionally render Navbar based on route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,14 @@
+import { useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
 
+import { useState } from "react";
 export default function App() {
+  const [ShowLogin, setShowLogin] = useState(false);
+  const isOwnerPath = useLocation().pathname.startsWith("/owner");
+
   return (
-    <div>
-      <Navbar />
-    </div>
+    <>
+      {!isOwnerPath && <Navbar setShowLogin={setShowLogin} />}
+    </>
   );
 }
