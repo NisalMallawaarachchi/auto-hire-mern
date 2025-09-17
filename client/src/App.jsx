a4c0715394410533ba4ba26ee969c456b6096eb7
NisalMallawaarachchi <nisalmallawarachchi@gmail.com>
2025-09-17 20:30:27 +0530

    feat: add routing and new pages for Home, Cars, CarDetails, and MyBookings

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,9 @@
-import { useLocation } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
+import Home from "./pages/Home.jsx";
+import Cars from "./pages/Cars.jsx";
+import CarDetails from "./pages/CarDetails.jsx";
+import MyBookings from "./pages/MyBookings.jsx";
 
 import { useState } from "react";
 export default function App() {
@@ -9,6 +13,15 @@ export default function App() {
   return (
     <>
       {!isOwnerPath && <Navbar setShowLogin={setShowLogin} />}
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/cars" element={<Cars/>}/>
+      <Route path="/car-details/:id" element={<CarDetails ShowLogin={ShowLogin} setShowLogin={setShowLogin}/>}/>
+      <Route path="/my-bookings" element={<MyBookings/>}/>
+    </Routes>
+
+
+
     </>
   );
 }
