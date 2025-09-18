a3c432c53e10195a3b24f8bac8572a2f842d760e
NisalMallawaarachchi <nisalmallawarachchi@gmail.com>
2025-09-18 14:34:07 +0530

    feat: enhance Hero component with pickup location selection and update Home import path

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,17 +1,44 @@
-import { assets } from "../assets/assets";
+import { useState } from "react";
+import { assets, cityList } from "../assets/assets";
 
-function Hero() {
+const Hero = () => {
+  const [pickupLocation, setPickupLocation] = useState("");
+
   return (
     <div className="h-screen flex flex-col items-center justify-center gap-14 bg-light text-center">
       <h1 className="text-4xl md:text-5xl font-semibold">
         Luxury cars on Rent
       </h1>
 
-      <form action=""></form>
+      <form
+        className="flex flex-col md:flex-row items-start md:items-center justify-between p-5 rounded-lg md:rounded-full w-full max-w-80 md:max-w-200 bg-white shadow-[0px_8px_20px_rgba(0,0,0,0.1)]"
+        action=""
+      >
+        <div className="flex flex-col md:flex-row items-start md:items-center gap-10 min-md:ml-8">
+          <div className="flex flex-col items-start gap-2">
+            <select
+              required
+              value={pickupLocation}
+              onChange={(e) => setPickupLocation(e.target.value)}
+              className="outline-none border-borderColor pb-2"
+            >
+              <option value="">Pickup Location</option>
+              {cityList.map((city) => (
+                <option key={city} value={city}>
+                  {city}
+                </option>
+              ))}
+            </select>
+            <p className="px-1 text-sm text-gray-500">
+              {pickupLocation ? pickupLocation : "Please select location"}
+            </p>
+          </div>
+        </div>
+      </form>
 
       <img src={assets.main_car} alt="car" className="max-h-74" />
     </div>
   );
-}
+};
 
 export default Hero;
