a922820b4e659415c9bfa07c4b8bceaf5e7503c2
NisalMallawaarachchi <nisalmallawarachchi@gmail.com>
2025-09-18 13:45:44 +0530

    feat: create hero section and add the main car

---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import { assets } from "../assets/assets";
+
+function Hero() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center gap-14 bg-light text-center">
+      <h1 className="text-4xl md:text-5xl font-semibold">
+        Luxury cars on Rent
+      </h1>
+
+      <form action=""></form>
+
+      <img src={assets.main_car} alt="car" className="max-h-74" />
+    </div>
+  );
+}
+
+export default Hero;
