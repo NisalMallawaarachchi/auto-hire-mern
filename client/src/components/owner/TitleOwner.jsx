65ce6c65643bbc37d84286ac23783e5c4937c953
NisalMallawaarachchi <nisalmallawarachchi@gmail.com>
2025-09-28 10:01:22 +0530

    feat: create ownder title component

--- client/src/components/owner/TitleOwner.jsx
+++ client/src/components/owner/TitleOwner.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
-const TitleOwner = () => {
+const TitleOwner = ({ title, subtitle }) => {
   return (
-    <div>TitleOwner</div>
-  )
-}
+    <>
+      <h1 className="font-medium text-3xl">{title}</h1>
+      <p className="text-sm md:text-base text-gray-500/90 mt-2 max-w-156">
+        {subtitle}
+      </p>
+    </>
+  );
+};
 
-export default TitleOwner
+export default TitleOwner;
